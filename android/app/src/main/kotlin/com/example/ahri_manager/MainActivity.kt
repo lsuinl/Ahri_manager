a2b502b1526c67fe8b61febe7a94e67570ac1658
lsuinl <su96in43@naver.com>
2022-09-10 22:00:56 +0900

    ptojcet produce

---
+++ android/app/src/main/kotlin/com/example/ahri_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ahri_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ahri_manager/screen/main.dart';
+
+void main(){
+  runApp(
+    MaterialApp( //앱 스타일
+    home: MainScreen(), //
+    ),
+  );
+}
