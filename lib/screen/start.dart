39b2ce16eed8e3aa118b6c9fb3badc5e666fdc7d
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2022-09-23 11:20:58 +0900

    feat: Add choose date, Button / refactor: fix design

--- lib/screen/start.dart
+++ lib/screen/start.dart
@@ -4,6 +4,8 @@
 이름, 성별, 생일, 몸무게, 중성화 여부
  */
 
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
@@ -17,16 +19,17 @@ class StartScreen extends StatefulWidget {
   State<StartScreen> createState() => _StartScreenState();
 }
 
-
 class _StartScreenState extends State<StartScreen> {
   TextEditingController name = TextEditingController();
-  TextEditingController birth = TextEditingController();
-  TextEditingController gender = TextEditingController();
   TextEditingController weight = TextEditingController();
-  TextEditingController blood = TextEditingController();
-  TextEditingController neutral = TextEditingController();
+
+  DateTime selectedDate = DateTime.now(); //기본값은 현재날짜로
 
   var userImage;
+  addUI(u) {
+    userImage = u;
+  }
+
   List _GenderType = ['수컷', '암컷'];
   List _SurgeryMenu = ['O', 'X'];
   List<DropdownMenuItem<String>> _dropDownGenderItems =
@@ -40,7 +43,8 @@ class _StartScreenState extends State<StartScreen> {
   void initState() {
     super.initState();
     for (var item in _GenderType) {
-      _dropDownGenderItems.add(DropdownMenuItem(value: item, child: Text(item)));
+      _dropDownGenderItems
+          .add(DropdownMenuItem(value: item, child: Text(item)));
     }
     for (var item in _SurgeryMenu) {
       _dropDownSurItems.add(DropdownMenuItem(value: item, child: Text(item)));
@@ -52,13 +56,13 @@ class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.lightGreen,
-          title: Text('동물 등록하기'),
-          centerTitle: true, //중간에 오도록
-          leading: Icon(Icons.home),
-        ),
-        body: ListView(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen,
+        title: Text('동물 등록하기'),
+        centerTitle: true, //중간에 오도록
+        leading: Icon(Icons.home),
+      ),
+      body: ListView(
           scrollDirection: Axis.vertical,
           padding: const EdgeInsets.all(10.0),
           children: <Widget>[
@@ -70,20 +74,25 @@ class _StartScreenState extends State<StartScreen> {
                     backgroundColor: MaterialStateProperty.all(Colors.green)),
                 onPressed: () async {
                   var picker = ImagePicker();
-                  var image =
+                  dynamic image =
                   await picker.pickImage(source: ImageSource.gallery);
 
                   if (image != null) {
                     setState(() {
-                      userImage = File(image.path);
+                      dynamic ui = File(image.path);
+                      addUI(ui);
                     });
+                  }
+                  Widget userima(){ //저장한 이미지를 띄우는 친구였어요... 새로운 페이지로 띄우는 건 가능했는데요? 이 페이지 안에 띄우는 건 못하겠어요.
+                    //서칭해서 찾아보니까 return Container(body:어쩌구)) 하던데 응 안돼
+                    return Image.file(userImage, height: 100, width: 100,);
                   }
                 },
               ),
             ),
-            //Image.file(userImage),
-            Padding(//이름
-              padding: EdgeInsets.fromLTRB(100, 10, 30, 30),
+            Padding(
+              //이름
+              padding: EdgeInsets.fromLTRB(100, 10, 30, 20),
               child: TextField(
                 controller: name,
                 decoration: InputDecoration(
@@ -91,33 +100,74 @@ class _StartScreenState extends State<StartScreen> {
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(100, 0, 30, 10),
-              child: Text('성별'),
-            ),
 
-            Padding(//성별
-              padding: EdgeInsets.fromLTRB(100, 10, 30, 5),
-              child: DropdownButton(
-                items: _dropDownGenderItems,
-                onChanged: (String? value) {
-                  setState(() {
-                    _genderbuttonText = value;
-                  });
-                },
-                value: _genderbuttonText,
+            Padding(
+              //성별
+              padding: EdgeInsets.fromLTRB(30, 5, 30, 10),
+              child: Row(
+                children: [
+                  Text('성별', style: TextStyle(fontSize: 17),),
+                  Text('          '),
+                  DropdownButton(
+                    items: _dropDownGenderItems,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _genderbuttonText = value;
+                      });
+                    },
+                    value: _genderbuttonText,
+                  ),
+                ],
               ),
             ),
 
-            Padding(//생일
-              padding: EdgeInsets.fromLTRB(100, 20, 30, 5),
+            Padding(
+              padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
               child: Row(
-                children: <Widget>[
-                  TextField(
-                    controller: birth,
-                    decoration: InputDecoration(
-                      labelText: 'Birth',
+                children: [
+                  Text(
+                    '생일',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  Text('      '),
+                  IconButton( //생일
+                    iconSize: 30.0,
+                    onPressed: () {
+                      showCupertinoDialog(
+                        context: context,
+                        barrierDismissible: true,
+                        builder: (BuildContext context) {
+                          return Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Container(
+                              color: Colors.white,
+                              height: 300.0,
+                              child: CupertinoDatePicker(
+                                mode: CupertinoDatePickerMode.date,
+                                initialDateTime: selectedDate,
+                                maximumYear: DateTime.now().year,
+                                maximumDate: DateTime.now(),
+                                onDateTimeChanged: (DateTime date) {
+                                  setState(() {
+                                    selectedDate = date; //사용자가 선택한 날짜 저장
+                                  });
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    icon: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
                     ),
+                  ),
+                  Text(
+                    '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20),
                   ),
                 ],
               ),
@@ -125,42 +175,72 @@ class _StartScreenState extends State<StartScreen> {
 
 
             Padding(//무게
-              padding: EdgeInsets.fromLTRB(100, 30, 30, 5),
-              child: TextField(
-                controller: weight,
-                decoration: InputDecoration(
-                  labelText: 'Weight',
-                ),
+              padding: EdgeInsets.fromLTRB(100, 10, 30, 0),
+              child: Column(
+                children: [
+                  Text('몸무게'),
+                  TextField(
+                    controller: weight,
+                    decoration: InputDecoration(
+                      labelText: 'Weight',
+                    ),
+                  ),
+                  Text(
+                    'Kg',
+                    textAlign: TextAlign.right,
+                  ),
+                ],
+              ),
+            ),
+
+
+            Padding(
+              //중성화여부
+              padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
+              child: Row(
+                children: [
+                  Text('중성화', style: TextStyle(fontSize: 17),),
+                  Text('        '),
+                  DropdownButton(
+                    items: _dropDownSurItems,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _neubuttonText = value;
+                      });
+                    },
+                    value: _neubuttonText,
+                  ),
+                ],
               ),
             ),
             Padding(
-              padding: EdgeInsets.fromLTRB(100, 30, 30, 5),
-              child: Text('중성화'),
-            ),
-            Padding(//중성화여부
-              padding: EdgeInsets.fromLTRB(100, 10, 30, 5),
-              child: DropdownButton(
-                items: _dropDownSurItems,
-                onChanged: (String? value) {
-                  setState(() {
-                    _neubuttonText = value;
-                  });
-                },
-                value: _neubuttonText,
+              padding: EdgeInsets.only(left: 100, right: 100),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    child: const Text('저장'),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.green)),
+                    onPressed: () { //홈화면으로 넘어가는 코드 이거 맞니? 이거 맞으면 /**/만 없애면 됨 굿~~
+                      /*Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      );
+                       */
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text('취소'),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.green)),
+                    onPressed: () {
+                      // 앱 종료 기능을 만들고 싶었어요.
+                    },
+                  ),
+                ],
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(15),
-              child: ElevatedButton(
-                child: const Text('저장'),
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.green)),
-                onPressed: () {
-                  //if
-                },
-              ),
-            ),
-          ],
-        ));
+          ]),
+    );
   }
 }
