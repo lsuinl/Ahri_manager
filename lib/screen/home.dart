6a7f3f0f3dfddcb7ab01b5d4db1150b2765e7fa2
zzzuuu <agu0830@naver.com>
2022-09-25 23:53:04 +0900

    before change background

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -17,104 +17,131 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightGreen,
-
-        title: Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Text(
-            "놀러와 우리홈",
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'jua',
-              fontSize: 30.0,
+        title:
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(
+                      "놀러와 우리홈",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'jua',
+                        fontSize: 30.0,
+                      ),
+                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50.0),
+                  child: ElevatedButton(
+                    child: Text(
+                      "설정",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(primary: Colors.white),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Buy_object()));
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-        ),
       ),
+
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
 
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          //crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
+            Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                IconButton( //병원 이미지
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Map_hospital()));
-                    },
-                    icon: Image.asset('imgs/hospital.png'),
-                    iconSize: 100,
-                  ),
-
-                Text(
-                      '병원 찾기',
-                      style: TextStyle(
-                        fontFamily: 'jua',
-                        fontSize: 25.0,
+                Column( //달력
+                    //mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Buy_object()));
+                        },
+                        icon: Image.asset('imgs/schedule.png'),
+                        iconSize: 70,
+                        //padding,
                       ),
-                    ),
+
+
+                      Text(
+                        "기록장",
+                        style: TextStyle(
+                          fontFamily: 'jua',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
               ],
             ),
 
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: IconButton( //물품 이미지
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Buy_object()));
-                    },
-                    icon: Image.asset('imgs/pet-food.png'),
-                    iconSize: 100,
-                  ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column( //병원 찾기
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton( //병원 이미지
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Map_hospital()));
+                        },
+                        icon: Image.asset('imgs/hospital.png'),
+                        iconSize: 70,
+                      ),
+
+                    Text(
+                          '병원 찾기',
+                          style: TextStyle(
+                            fontFamily: 'jua',
+                            fontSize: 20.0,
+                          ),
+                        ),
+                  ],
                 ),
-
-
-                Text(
-                  "물품구매",
-                  style: TextStyle(
-                    fontFamily: 'jua',
-                    fontSize: 25.0,
-                  ),
-                ),
-
-            IconButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Buy_object()));
-                    },
-                    icon: Image.asset('imgs/schedule.png'),
-                    iconSize: 100,
-                    //padding,
-                  ),
-
-
-                Text(
-                  "기록장",
-                  style: TextStyle(
-                    fontFamily: 'jua',
-                    fontSize: 25.0,
-                  ),
-                ),
-
-
-            //설정으로
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: ElevatedButton(
-                child: Text(
-                  "설정",
-                  style: TextStyle(color: Colors.black),
-                ),
-                style: ElevatedButton.styleFrom(primary: Colors.white),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Buy_object()));
-                },
-              ),
+              ],
             ),
+
+
+            //동물 이미지 넣기 (Column)
+
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    IconButton( //물품 이미지
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Buy_object()));
+                        },
+                      icon: Image.asset('imgs/pet-food.png'),
+                      iconSize: 70,
+                    ),
+
+                    Text(
+                      "물품구매",
+                      style: TextStyle(
+                        fontFamily: 'jua',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
           ],
         ),
 
