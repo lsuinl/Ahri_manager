e219daaec37affa5ed522ff24842268d6bc1795d
lsuinl <su96in43@naver.com>
2022-09-27 23:15:45 +0900

    feat: lodding.dart 권한설정 및 시작버튼

--- lib/screen/lodding.dart
+++ lib/screen/lodding.dart
@@ -1,5 +1,10 @@
+import 'package:ahri_manager/screen/home.dart';
+import 'package:ahri_manager/screen/start.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:ahri_manager/data/user_data.dart';
+import 'package:ahri_manager/plus/user_helper.dart';
+
 class Lodding extends StatefulWidget {
   const Lodding({Key? key}) : super(key: key);
 
@@ -8,61 +13,106 @@ class Lodding extends StatefulWidget {
 }
 
 class _LoddingState extends State<Lodding> {
+  List<user_information> user_infotmations = [];
+  final UserHelper helper = UserHelper();
+
+  @override
+  void initState() {
+    helper.init().then((value) {
+      updateScreen();
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            '/Users/kimsu-in/Desktop/icecream/flutter-sw/imgs/unicorn.png',
-          ),
-          CircularProgressIndicator(
-            color: Colors.white,
-          )
-        ],
-      ),
+      body: FutureBuilder<String>(
+          future: checkpermission(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == '권한이 허가되었습니다.') {
+              //권한설정이 완료되었는지.
+              //초기설정완료된 상태인지.
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    '/Users/kimsu-in/Desktop/icecream/flutter-sw/asset/imgs/unicorn.png',
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      "시작하기",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(),
+                    onPressed: () {
+                      if (user_infotmations.length<1) { //정보저장여부
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => StartScreen()));
+                      } else {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen()));
+                      }
+                    },
+                  ),
+                ],
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    '/Users/kimsu-in/Desktop/icecream/flutter-sw/asset/imgs/unicorn.png',
+                  ),
+                  CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "데이터를 확인 중입니다.",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              );
+            } else
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    '/Users/kimsu-in/Desktop/icecream/flutter-sw/asset/imgs/unicorn.png',
+                  ),
+                  Text(
+                    "권한 설정에 문제가 생겼습다. 권한을 설정해주세요.",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              );
+          }),
     );
-    // if() 정보가 저장되어 있는경우 home
+  }
 
-    // 없는 경우 start
+  void updateScreen() {
+    user_infotmations = helper.getuserinformation();
+    setState(() {});
   }
 }
-//     return Scaffold(
-//       backgroundColor: Colors.lightBlueAccent,
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Image.asset(
-//             '/Users/kimsu-in/Desktop/icecream/flutter-sw/imgs/unicorn.png',
-//           ),
-//           CircularProgressIndicator(
-//             color: Colors.white,
-//           )
-//         ],
-//       ),
-//     );
-//
-// Future<String> checkPermission() async {
-//   //권한을 사용할 수 있는지를 확인
-//   final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
-//
-//   if (!isLocationEnabled) {
-//     return '위치 서비스를 활성화 해주세요.';
-//   }
-//   //현재 앱이 가지고 있는 위치서비스에 대한 권한 값을 location이라는 형식으로 반환
-//   LocationPermission checkPermission = await Geolocator.checkPermission();
-//   if (checkPermission == LocationPermission.denied) {
-//     //권한을 사용할 수 없지만 요청할 수는 있는 경우
-//     checkPermission = await Geolocator.requestPermission();
-//
-//     if (checkPermission == LocationPermission.denied) {
-//       return '위치 권한을 허가해주세요';
-//     }
-//   }
-//   if (checkPermission == LocationPermission.deniedForever) {
-//     return '앱의 위치 권한을 셋팅에서 허가해주세요';
-//   }
-//   return '위치 권한이 허가되었습니다.';
-// }
+
+Future<String> checkpermission() async {
+  Permission.locationWhenInUse.request();
+  Permission.photos.request();
+  if ((await Permission.photos.isGranted ||
+      await Permission.photos.isLimited)&&(
+      await Permission.locationWhenInUse.isGranted ||
+          await Permission.locationWhenInUse.isLimited
+  )) {
+    //권한확인
+    return '권한이 허가되었습니다.';
+  } else {
+    return '권한에 문제가 발생했습니다.';
+  }
+}
