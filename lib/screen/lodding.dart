199aff340c25f8844ba985301234d122e69a5e47
lsuinl <su96in43@naver.com>
2022-09-29 01:51:59 +0900

    refactor: lodding.dart

--- lib/screen/lodding.dart
+++ lib/screen/lodding.dart
@@ -7,7 +7,6 @@ import 'package:ahri_manager/plus/user_helper.dart';
 
 class Lodding extends StatefulWidget {
   const Lodding({Key? key}) : super(key: key);
-
   @override
   State<Lodding> createState() => _LoddingState();
 }
@@ -18,9 +17,7 @@ class _LoddingState extends State<Lodding> {
 
   @override
   void initState() {
-    helper.init().then((value) {
-      updateScreen();
-    });
+    helper.init().then((value) {updateScreen();});
     super.initState();
   }
 
@@ -31,69 +28,13 @@ class _LoddingState extends State<Lodding> {
       body: FutureBuilder<String>(
           future: checkpermission(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.data == '권한이 허가되었습니다.') {
-              //권한설정이 완료되었는지.
-              //초기설정완료된 상태인지.
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    '/Users/kimsu-in/Desktop/icecream/flutter-sw/asset/imgs/unicorn.png',
-                  ),
-                  ElevatedButton(
-                    child: Text(
-                      "시작하기",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    style: ElevatedButton.styleFrom(),
-                    onPressed: () {
-                      if (user_infotmations.length<1) { //정보저장여부
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => StartScreen()));
-                      } else {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => HomeScreen()));
-                      }
-                    },
-                  ),
-                ],
-              );
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    '/Users/kimsu-in/Desktop/icecream/flutter-sw/asset/imgs/unicorn.png',
-                  ),
-                  CircularProgressIndicator(
-                    color: Colors.white,
-                  ),
-                  Text(
-                    "데이터를 확인 중입니다.",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ],
-              );
-            } else
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    '/Users/kimsu-in/Desktop/icecream/flutter-sw/asset/imgs/unicorn.png',
-                  ),
-                  Text(
-                    "권한 설정에 문제가 생겼습다. 권한을 설정해주세요.",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ],
-              );
-          }),
-    );
+            if (snapshot.data == '허가')
+              return _ok(user_infotmations: user_infotmations,);
+            if (snapshot.connectionState == ConnectionState.waiting)
+              return _lodding();
+            else
+              return _error();
+          }));
   }
 
   void updateScreen() {
@@ -103,16 +44,91 @@ class _LoddingState extends State<Lodding> {
 }
 
 Future<String> checkpermission() async {
-  Permission.locationWhenInUse.request();
-  Permission.photos.request();
-  if ((await Permission.photos.isGranted ||
-      await Permission.photos.isLimited)&&(
-      await Permission.locationWhenInUse.isGranted ||
-          await Permission.locationWhenInUse.isLimited
-  )) {
-    //권한확인
-    return '권한이 허가되었습니다.';
-  } else {
-    return '권한에 문제가 발생했습니다.';
+  var locationper=Permission.locationWhenInUse.request();
+  var photoper= Permission.photos.request();
+  if(locationper.isDenied==true) Permission.locationWhenInUse.request();
+  if(photoper.isDenied==true) Permission.locationWhenInUse.request();
+  if((await locationper.isGranted==false||await locationper.isLimited==false) &&
+      await photoper.isGranted==false ||await photoper.isLimited==false )
+    return '허가';
+  else
+    return '문제 발생';
+}
+
+class _ok extends StatelessWidget {
+  final List<user_information> user_infotmations;
+
+  const _ok({
+    required this.user_infotmations,
+    Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'asset/imgs/unicorn.png',
+        ),
+        ElevatedButton(
+          child: Text(
+            "시작하기",
+            style: TextStyle(color: Colors.white),
+          ),
+          style: ElevatedButton.styleFrom(),
+          onPressed: () {
+            if (user_infotmations.length<1) //정보저장여부
+              Navigator.push(context, MaterialPageRoute(builder: (context) => StartScreen()));
+            else
+              Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+          },
+        ),
+      ],
+    );
   }
 }
+
+
+class _lodding extends StatelessWidget {
+  const _lodding({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'asset/imgs/unicorn.png',
+        ),
+        CircularProgressIndicator(
+          color: Colors.white,
+        ),
+        Text(
+          "데이터를 확인 중입니다.",
+          style: TextStyle(color: Colors.black),
+        ),
+      ],
+    );
+  }
+}
+
+class _error extends StatelessWidget {
+  const _error({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'asset/imgs/unicorn.png',
+        ),
+        Text(
+          "권한 설정에 문제가 생겼습다. 권한을 설정해주세요.",
+          style: TextStyle(color: Colors.black),
+        ),
+      ],
+    );
+  }
+}
+
