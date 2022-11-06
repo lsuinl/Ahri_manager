e76ca756fa4a997e1ff11c0b9efb76b4cbd55d97
lsuinl <su96in43@naver.com>
2022-11-06 23:41:41 +0900

    feat: custom marker, refactoring code

--- lib/screen/naver_map.dart
+++ lib/screen/naver_map.dart
@@ -1,5 +1,4 @@
-import 'dart:async';
-
+// import 'dart:async';
 import 'package:ahri_manager/data/user_information.dart';
 import 'package:ahri_manager/data/hospital_information.dart';
 import 'package:ahri_manager/plus/user_helper.dart';
@@ -19,97 +18,112 @@ class naver extends StatefulWidget {
 }
 
 class _naverState extends State<naver> {
-  List<Marker> _markers=[];
+  List<Marker> _markers = [];
   NaverMapController? mapController;
   List<information> hospitalinf = []; //이거왜?
   List<user_information> user_informations = [];
   final UserHelper helper = UserHelper();
   LatLng mylocation = LatLng(0, 0);
-  Uint8List markerIcon=Uint8List(0);//초기화
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  // Completer<NaverMapController> _controller = Completer();//
+  late OverlayImage markerIcon;
 
-  final scaffoldKey = GlobalKey<ScaffoldState>(); //
-  Completer<NaverMapController> _controller = Completer();//
+  void setCustomMapPin() async {
+    markerIcon = await OverlayImage.fromAssetImage(
+        assetName: 'asset/imgs/hospitalmarker.png',
+        size: Size(1, 1)
+    );
+  }
 
   @override
-  void initState(){
-    hospitalinf=hospitialinf;
-    helper.init().then((value) {updateScreen();});
+  void initState() {
+    hospitalinf = hospitialinf;
+    helper.init().then((value) {
+      updateScreen();
+    });
     getCurrentLocation();
+    setCustomMapPin();
     super.initState();
   }
 
   @override
+  //정보 가져오기+마커화
   Widget build(BuildContext context) {
     String animalspecies = "";
-    if(user_informations.isNotEmpty){
+    if (user_informations.isNotEmpty) {
       animalspecies = user_informations.first.species;
     }
 
-    for(int i=0;i<hospitalinf.length;i++){
-      if(hospitalinf[i].animal.contains(animalspecies)){
+    for (int i = 0; i < hospitalinf.length; i++) {
+      if (hospitalinf[i].animal.contains(animalspecies)) {
         _markers.add(Marker(
-            markerId: (hospitalinf[i].name),
-            // icon: BitmapDescriptor.fromBytes(markerIcon),
-            position: LatLng(
-              hospitalinf[i].xy.latitude,
-              hospitalinf[i].xy.longitude,
-            ),
-            onMarkerTab: (Marker? marker, Map<String, int?> iconSize){
-        showModalBottomSheet<void>(
-        context: context,
-        builder: (context) {
-        return Container(
-        height: 200,
-        decoration: BoxDecoration(
-        image: DecorationImage(
-        image: AssetImage('asset/imgs/pattern1.png'),
-        fit: BoxFit.cover,
-        ),
-        ),
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-        Text(
-        "${hospitalinf[i].name}",
-        style: TextStyle(
-        fontFamily: 'jua',
-        fontSize: 20.0,
-        color: Colors.brown,
-        ),
-        ),
-        new TextButton(
-        onPressed: () => launchUrl(Uri.parse(
-        'tel:${hospitalinf[i].phone.replaceAll("-", "")}')),
-        child: new Text(
-        "${hospitalinf[i].phone}",
-        style: TextStyle(
-        fontSize: 16,
-        fontFamily: 'jua',
-        color: Colors.red,
-        ),
-        ),
-        ),
-        Text(
-        "${hospitalinf[i].adress}",
-        textAlign: TextAlign.center,
-        style: TextStyle(
-        fontFamily: 'jua',
-        fontSize: 20.0,
-        color: Colors.brown,
-        ),
-        ),
-        ],
-        ),
-        //),
-        );
-        }, // builder
-        );
-        },
-          infoWindow:hospitalinf[i].name,
+          markerId: (hospitalinf[i].name),
+          // icon: BitmapDescriptor.fromBytes(markerIcon),
+          position: LatLng(
+            hospitalinf[i].xy.latitude,
+            hospitalinf[i].xy.longitude,
+          ),
+          icon: markerIcon,
+          onMarkerTab: (Marker? marker, Map<String, int?> iconSize) {
+            showModalBottomSheet<void>(
+              context: context,
+              builder: (context) {
+                return Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('asset/imgs/pattern1.png',),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        "${hospitalinf[i].name}",
+                        style: TextStyle(
+                          fontFamily: 'jua',
+                          fontSize: 20.0,
+                          color: Colors.brown,
+                        ),
+                      ),
+                      new TextButton(
+                        onPressed: () =>
+                            launchUrl(Uri.parse(
+                                'tel:${hospitalinf[i].phone.replaceAll(
+                                    "-", "")}')),
+                        child: new Text(
+                          "${hospitalinf[i].phone}",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'jua',
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "${hospitalinf[i].adress}",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'jua',
+                          fontSize: 20.0,
+                          color: Colors.brown,
+                        ),
+                      ),
+                    ],
+                  ),
+                  //),
+                );
+              }, // builder
+            );
+          },
+          infoWindow: hospitalinf[i].name,
         ));
       }
     }
+
+    //화면구성
     return Scaffold(
         appBar: AppBar(
           title: Text(
@@ -158,7 +172,7 @@ class _naverState extends State<naver> {
             ),
           ],
         ),
-        body:StreamBuilder<Position>(
+        body: StreamBuilder<Position>(
             stream: Geolocator.getPositionStream(), //내 현재위치 가져오기
             builder: (context, snapshot) {
               return Column(
@@ -169,10 +183,14 @@ class _naverState extends State<naver> {
                       initialCameraPosition: CameraPosition(
                         target: LatLng(37.564214, 127.001699),
                         zoom: 11,
-                      ),       //초기 카메라 위치
-                      logoClickEnabled: true,     //내위치표시
-                      locationButtonEnable: true,     //내위치로가기버튼
-                      mapType: MapType.Basic,  //맵타입형식
+                      ),
+                      //초기 카메라 위치
+                      logoClickEnabled: true,
+                      //내위치표시
+                      locationButtonEnable: true,
+                      //내위치로가기버튼
+                      mapType: MapType.Basic,
+                      //맵타입형식
                       // onMapCreated: _onMapCreated(mapController!),    //콜백 조작
                       markers: _markers,
                     ),
@@ -182,11 +200,12 @@ class _naverState extends State<naver> {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => HospitalListScreen(
-                                mylocation: mylocation,
-                              )
+                              builder: (context) =>
+                                  HospitalListScreen(
+                                    mylocation: mylocation,
+                                  )
                           ));
-                      },
+                    },
                     child: Text(
                       "리스트로 보기",
                       style: TextStyle(
@@ -206,6 +225,7 @@ class _naverState extends State<naver> {
   //   _controller.complete(controller);
   // }
 
+  //현재위치 가져오기
   getCurrentLocation() async {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low);
@@ -215,6 +235,7 @@ class _naverState extends State<naver> {
     });
   }
 
+  //초기상태업데이트
   void updateScreen() {
     user_informations = helper.getuserinformation();
     setState(() {});
