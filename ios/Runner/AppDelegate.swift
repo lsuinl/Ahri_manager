75e978a6b8e79329c1b4408ecd9e4434a01eccd1
lsuinl <su96in43@naver.com>
2024-10-24 12:50:46 +0900

    refactor: flutter version update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,23 +1,27 @@
 import UIKit
 import Flutter
+import CoreLocation // CLLocationManager를 사용하기 위해 CoreLocation을 import
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
+  let manager = CLLocationManager() // CLLocationManager 인스턴스 생성
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // CLLocationManager 사용 권한 요청을 메소드 내부로 이동
+    if CLLocationManager.locationServicesEnabled() {
+        switch CLLocationManager.authorizationStatus() {
+        case .denied, .notDetermined, .restricted:
+            self.manager.requestAlwaysAuthorization()
+        default:
+            break
+        }
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}
-
-if (CLLocationManager.locationServicesEnabled()) {
-    switch CLLocationManager.authorizationStatus() {
-    case .denied, .notDetermined, .restricted:
-        self.manager.requestAlwaysAuthorization()
-        break
-    default:
-        break
-    }
 }
